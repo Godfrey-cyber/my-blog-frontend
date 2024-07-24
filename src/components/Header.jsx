c131343d53a02c7c4c70702a42275c1baa37c206
Godfrey_n <65883099+Godfrey-cyber@users.noreply.github.com>
2024-07-24 11:07:10 +0300

    Updated Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react'
 import { UserContext } from "../UserContext"
 import { Link } from "react-router-dom"
 import { client } from "../assets/utilities.js"
+import axios from 'axios'
 
 const Header = () => {
 	const { setUserData, userData } = useContext(UserContext)
@@ -13,9 +14,14 @@ const Header = () => {
 				const response = await client.get("/users/profile", {
 					withCredentials: true
 				})
-				setUserData(response?.data);
+				if (response && response?.status === 200 || response.statusText === "OK") {
+					setUserData(response?.data);
+				}
 			} catch (error) {
-				console.error(error?.stack);
+				if (error || !response?.status === 200 || !response?.statusText === 'OK') {
+					console.error(error?.stack);
+					console.error('❗Error fetching data❌:', error.message);
+				}
 			}
 		}
 		getUserProfile()
