13041708a1b0145ca6568f9209b39b7a8d331ce5
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-12-19 12:22:31 +0300

    refactored login code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,12 +19,13 @@ const Login = () => {
 		try {
 		const res = await axios.post("https://my-blog-backend-t19h.onrender.com/users/login", formData, { withCredentials: true })
 			if (res.status === 200 || res.status === 201) {
-				setFormData({ email: "", password: "" });
+				setPassword("")
+				setEmail("")
 				navigate("/")
 	   			// toast.success("Successfully Logged in🥇")
 			}
 		} catch (error) {
-			console.log(err)
+			console.log(error)
 			setPassword("")
 		}
 	}
